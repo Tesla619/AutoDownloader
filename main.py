ef4fd0ed6bc939fe9a885431c089f593c39580b8
Karl Spiteri <karl.spiteri.e21724@mcast.edu.mt>
2022-09-26 12:30:32 +0200

    Update: chrome driver in dir & no DeprecWarnings

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import os
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
 chrome_options.add_extension("uBlock-Origin.crx")
 
-#Update To Do: if condition for install if first time or driver needs updating
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+#taskkill /IM chromedriver.exe /F
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager(path = "Drivers").install()), options = chrome_options)
 
 EPLINK = "?ep="
 anime1 = "https://9anime.tube/watch/detective-conan/"
@@ -15,14 +18,11 @@ anime2 = "https://9anime.tube/watch/boruto-naruto-next-generations/"
 anime3 = "https://9anime.tube/watch/one-piece-episode-of-merry-mou-hitori-no-nakama-no-monogatari/"
 anime4 = "https://9anime.tube/watch/pokemon-2019/"
 
-def custom_Ep_fullXpath(ep):
-    return """/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[2]/div/ul/li["""+ str(ep) +"""]/a"""
-
 def custom_Ep_Xpath(ep):
     return """//*[@id="list-eps"]/li["""+str(ep)+"""]/a"""
 
 def delay_click(xpath, delay):
-    driver.find_element("xpath",xpath).click
+    driver.find_element("xpath",xpath).click()
     time.sleep(delay)
 
 def download(url,ep):
@@ -34,8 +34,3 @@ def download(url,ep):
 #driver.switch_to.window
 
 #driver.get(anime1) FILLER 
-
-download(anime1,1)
-#driver.switch_to.frame()
-#delay_click(custom_Ep_Xpath(1059), 1)
-delay_click(custom_Ep_fullXpath(1059), 1)
