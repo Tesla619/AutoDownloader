50430b2dde50545c34f1e6dc705da448262597fd
Karl Spiteri <karl.spiteri.e21724@mcast.edu.mt>
2022-09-26 08:55:28 +0200

    progress1

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-optObj = Options()
-optObj.headless = True
-
-#if condition for install if first time or driver needs updating
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-#driver = webdriver.Chrome(options = optObj)
+#Update To Do: if condition for install if first time or driver needs updating
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+def google(): #using func doesn't auto close browser
+    driver.get('https://www.google.com')
 
-driver = webdriver.Chrome(options=options, executable_path="path/to/executable")
-driver.get('https://www.google.com')
+#if __name__ == "__main__":
+google()
+    
+    
